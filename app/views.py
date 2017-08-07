6fd3ec508e99328a07ec910f4df323f162905eef
Adriel Arsenault <adriel.arsenault@gmail.com>
2017-08-07 19:38:37 -0400

    mock up of profile page

--- app/views.py
+++ app/views.py
@@ -1,8 +1,46 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/profile')
+
+
+
+def profile():
+    user = {'name': 'Nasreen'}  # fake user
+    links = [  # fake array of posts
+        {
+            'author': user,
+            'date' : "04-08-2017",
+            'description': 'cool information site',
+            'link': 'https://www.wikipedia.org',
+            'tags': None
+        },
+
+        {
+            'author': user,
+            'date' : "06-08-2017",
+            'description': 'worst site I\'ve ever used',
+            'link': 'www.facebook.org',
+            'tags': None
+        },
+
+        {
+            'author': user,
+            'date' : "01-08-2017",
+            'description': 'other cool site',
+            'link': 'www.python.org',
+            'tags': None
+        }
+
+    ]
+    return render_template("profile.html",
+                           user=user,
+                           links=links)
